0710b73184440a43c7ea16808a63f81b9a4b956d
EricchiYukia <git@ericchi.moe>
2022-12-12 16:28:36 +0100

    C’è qualcosa.

---
+++ ListaIndirizzi/Indirizzo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace ListaIndirizzi
+{
+	public class Indirizzo
+	{
+		private string name;
+		private string surname;
+		private string street;
+		private string city;
+		private string province;
+		private string zip;
+
+		public Indirizzo (string name, string surname, string street, string city, string province, string zip) {
+			this.name = name;
+			this.surname = surname;
+			this.street = street;
+			this.city = city;
+			this.province = province;
+			this.zip = zip;
+		}
+	}
+}
+
---
+++ ListaIndirizzi/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ListaIndirizzi;
+
+Console.WriteLine("Hello, World!");
+
+List<Indirizzo> listaIndirizzi = new List<Indirizzo>();
+
+try
+{
+	StreamReader file = File.OpenText("Files/addresses.csv");
+
+	while (!file.EndOfStream)
+	{
+		string riga = file.ReadLine();
+
+		string[] infoIndirizzo = riga.Split(",");
+
+		string name = infoIndirizzo[0];
+		string surname = infoIndirizzo[1];
+		string street = infoIndirizzo[2];
+		string city = infoIndirizzo[3];
+		string province = infoIndirizzo[4];
+		string zip = infoIndirizzo[5];
+
+
+		Indirizzo indirizzoEstratto = new Indirizzo(name, surname, street, city, province, zip);
+		listaIndirizzi.Add(indirizzoEstratto);
+		// Console.WriteLine(riga);
+	}
+
+}
+catch (Exception e)
+{
+	Console.WriteLine(e.Message);
+}
